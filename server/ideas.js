69fbb647cdbdf8824abb13d4dc2611fb1d72ab37
Samuel Symonds <samuelsymonds2017@gmail.com>
2021-04-07 15:19:28 +0100

    Set path to get idea by id

--- server/ideas.js
+++ server/ideas.js
@@ -10,7 +10,29 @@ const {
   deleteFromDatabasebyId
 } = require('./db');
 
+// If params includes id, set the req.idea as the idea or send back a 404 status
+ideasRouter.param('id', (req, res, next, id) => {
+  const idea = getFromDatabaseById('ideas', id);
+  if (idea) {
+    req.idea = idea;
+    next();
+  } else {
+    res.status(404).send();
+  }
+});
+
 // GET all ideas
 ideasRouter.get('/', (req, res, next) => {
   res.send(getAllFromDatabase('ideas'));
+});
+
+// GET idea by ID
+ideasRouter.get('/:id', (req, res, next) => {
+  res.send(req.idea);
+});
+
+// POST idea to database
+ideasRouter.post('/', (req, res, next) => {
+  const newIdea = addToDatabase('ideas', req.body);
+  res.status(201).send(newIdea);
 });
