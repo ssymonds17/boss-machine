6b37a5afe08294d678921a3137620c6a8c2f35b9
Samuel Symonds <samuelsymonds2017@gmail.com>
2021-04-07 14:37:29 +0100

    Import helper functions from db.js into minions.js

--- server/minions.js
+++ server/minions.js
@@ -1,3 +1,12 @@
 const minionsRouter = require('express').Router();
 
+const {
+  getAllFromDatabase,
+  getFromDatabaseById,
+  addToDatabase,
+  updateInstanceInDatabase,
+  deleteFromDatabasebyId,
+  deleteAllFromDatabase
+} = require('./db');
+
 module.exports = minionsRouter;
