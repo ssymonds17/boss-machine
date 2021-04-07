2a87e5c9b57f9a2136a0f95647bf91b08b4ad7a0
Samuel Symonds <samuelsymonds2017@gmail.com>
2021-04-07 14:45:02 +0100

    Set initial minion get request for ALL minions. Also write reusable code so that if param includes minionId then set req.minion to that value or send back 404 status

--- server/minions.js
+++ server/minions.js
@@ -1,5 +1,7 @@
 const minionsRouter = require('express').Router();
 
+module.exports = minionsRouter;
+
 const {
   getAllFromDatabase,
   getFromDatabaseById,
@@ -9,4 +11,20 @@ const {
   deleteAllFromDatabase
 } = require('./db');
 
-module.exports = minionsRouter;
+// If params includes minionId, set the req.minion as the minion or send back a 404 status
+minionsRouter.param('minionId', (req, res, next, id) => {
+  const minion = getFromDatabaseById('minions', id);
+  if (minion) {
+    req.minion = minion;
+    next();
+  } else {
+    res.status(404).send();
+  }
+});
+
+// GET all minions
+minionsRouter.get('/', (req, res, next) => {
+  res.send(getAllFromDatabase('minions'));
+});
+
+// GET minion by ID
